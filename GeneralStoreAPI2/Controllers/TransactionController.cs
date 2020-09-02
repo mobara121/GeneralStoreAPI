53356c865c1e31ba73ea23cf1f15574ba79dc0d7
mobara121 <mizueo121@gmail.com>
2020-09-02 08:34:37 -0400

    Transaction crud updated

--- GeneralStoreAPI2/Controllers/TransactionController.cs
+++ GeneralStoreAPI2/Controllers/TransactionController.cs
@@ -60,9 +60,76 @@ namespace GeneralStoreAPI2.Controllers
 
         }
         //Get a transaction by its ID (GET)
+        //[HttpGet]
+        //public async Task<IHttpActionResult> GetByTransactionId(int Id)
+        //{
+        //    Product transactions = await _context.Product.FindAsync(Id);
+
+        //    if (transactions == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(transactions);
+
+        //}
 
         //Update by its ID (PUT)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateTransaction([FromUri]int CustomerId, [FromBody]Transaction updateTransaction)
+        {
+            if (ModelState.IsValid)
+            {
+                Transaction transaction = await _context.Transaction.FindAsync(CustomerId);
+                Customer customer = await _context.Customer.FindAsync(updateTransaction.CustomerId);
+                Product product = await _context.Product.FindAsync(updateTransaction.ProductSKU);
 
+                if (transaction != null)
+                {
+                    if (customer == null)
+                    {
+                        return BadRequest("Customer not found.");
+                    }
+                    if (product == null)
+                    {
+                        return BadRequest("Product not found.");
+                    }
+                    transaction.ProductSKU = updateTransaction.ProductSKU;
+                    transaction.Product = updateTransaction.Product;
+                    
+                    if (updateTransaction.ItemCount > product.NumberInInventory) //important!!
+                    {
+                        return BadRequest($"Not Enough {product.Name} items in stock.\n" +
+                            $"Please enter a quantity less than {product.NumberInInventory + 1} ");
+                    }
+                    transaction.ItemCount = updateTransaction.ItemCount;
+
+                    await _context.SaveChangesAsync();
+
+                    return Ok("Transaction has been updated.");
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
+        }
         //Delete by its ID (DELETE)
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteTransactionById(int CustomerId)
+        {
+            Transaction transaction = await _context.Transaction.FindAsync(CustomerId);
+
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            _context.Transaction.Remove(transaction);
+
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return Ok("The transaction was deleted.");
+            }
+
+            return InternalServerError();
+        }
     }
 }
